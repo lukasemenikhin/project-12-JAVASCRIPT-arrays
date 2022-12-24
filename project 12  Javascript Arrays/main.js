ae987d02b6a074d28b5b9978b65a000608050f2a
Luka Semenikhin <117368298+lukasemenikhin@users.noreply.github.com>
2022-12-24 21:57:58 +0400

    Add files via upload

---
+++ project 12  Javascript Arrays/main.js
@@ -0,0 +1,40 @@
+const friend1 = "ana";
+const friend2 = "nika";
+const friend3 = "sandro";
+
+const friends = ["ana","nika", "sandro"];
+console.log(friends);
+
+// console.log(friends);
+
+const years = new Array(1992, 1993, 1999);
+console.log(years);
+
+console.log(friends.length);
+
+
+// const friends = ["ana", "nika", "sandro"];
+// console.log(friends[friends.length -1]);
+
+// const firstname = "ana";
+
+// const friends = [firstname, "gvari", 2022 - 1992];
+// console.log(friends);
+
+function calcAge(birthyear) {
+    console.log(2022 - birthyear);
+
+}
+
+calcAge(1992);
+calcAge(1993);
+calcAge(1999);
+
+
+// const years =[1990, 1967, 1996, 2004, 2001];
+
+
+// years.push(2003);
+// years.unshift(1981);
+
+// console.log(years);
